16003a37c01f72c6172ed8e94026488d07f75488
Sahil <sahil.borse@centralogic.net>
2024-09-01 01:49:17 +0530

    config

--- backend/models/address.model.js
+++ backend/models/address.model.js
@@ -1,7 +1,12 @@
 import mongoose from "mongoose";
 
-const addressSchema = new mongoose.Schema({
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+const addressSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
     fullName: { type: String, required: true },
     addressLine1: { type: String, required: true },
     addressLine2: { type: String },
@@ -9,7 +14,9 @@ const addressSchema = new mongoose.Schema({
     state: { type: String, required: true },
     postalCode: { type: String, required: true },
     country: { type: String, required: true },
-    phoneNumber: { type: String, required: true }
-}, { timestamps: true });
+    phoneNumber: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
 export const Address = mongoose.model("Address", addressSchema);
